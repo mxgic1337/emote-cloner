ec3d882e80a0d8830cea613eecf80532e23fc384
mxgic1337_ <60188749+mxgic1337@users.noreply.github.com>
2023-11-17 17:41:04 +0100

    Changed regular expressions in URLUtil.ts

--- util/URLUtil.ts
+++ util/URLUtil.ts
@@ -2,26 +2,26 @@
 export function isBTTVURLValid(url: string, urlType: 'emotes' | 'users') {
     switch (urlType) {
         default:
-            return /^((http|https):\/\/)(www.|)betterttv.com\/emotes\/[a-zA-Z0-9]{24}/gi.test(url)
+            return /^((http|https):\/\/)(www\.|)betterttv\.com\/emotes\/[a-zA-Z0-9]{24}(\/|)$/gi.test(url)
         case 'users':
-            return /^((http|https):\/\/)(www.|)betterttv.com\/users\/[a-zA-Z0-9]{24}/gi.test(url)
+            return /^((http|https):\/\/)(www\.|)betterttv\.com\/users\/[a-zA-Z0-9]{24}(\/|)$/gi.test(url)
     }
 }
 
 /** Checks if provided URL points to FrankerFaceZ */
 export function isFFZURLValid(url: string) {
-    return /^((http|https):\/\/)(www.|)frankerfacez.com\/emoticon\/*/gi.test(url)
+    return /^((http|https):\/\/)(www\.|)frankerfacez\.com\/emoticon\/[0-9]{6}-[a-zA-Z0-9]*(\/|)$/gi.test(url)
 }
 
 /** Checks if provided URL points to 7TV */
 export function is7TVURLValid(url: string, urlType: 'emotes' | 'users' | 'emote-sets') {
     switch (urlType) {
         default:
-            return /^((http|https):\/\/)(www.|)7tv.app\/emotes\/[a-zA-Z0-9]{24}/gi.test(url)
+            return /^((http|https):\/\/)(www\.|)7tv\.app\/emotes\/[a-zA-Z0-9]{24}(\/|)$/gi.test(url)
         case 'users':
-            return /^((http|https):\/\/)(www.|)7tv.app\/users\/[a-zA-Z0-9]{24}/gi.test(url)
+            return /^((http|https):\/\/)(www\.|)7tv\.app\/users\/[a-zA-Z0-9]{24}(\/|)$/gi.test(url)
         case 'emote-sets':
-            return /^((http|https):\/\/)(www.|)7tv.app\/emote-sets\/[a-zA-Z0-9]{24}/gi.test(url)
+            return /^((http|https):\/\/)(www\.|)7tv\.app\/emote-sets\/[a-zA-Z0-9]{24}(\/|)$/gi.test(url)
     }
 }
 
